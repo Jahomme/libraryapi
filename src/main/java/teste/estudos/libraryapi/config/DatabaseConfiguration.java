e366e086efb1a6212ac9071f58c701ea50f653fc
Jadilson <jadilsonigor@gmail.com>
2025-11-11 15:28:29 -0300

    Initial commit

---
+++ src/main/java/teste/estudos/libraryapi/config/DatabaseConfiguration.java
@@ -0,0 +1,41 @@
+package teste.estudos.libraryapi.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+public class DatabaseConfiguration {
+
+    @Value("${spring.datasource.url}")
+    String url;
+    @Value("${spring.datasource.username}")
+    String username;
+    @Value("${spring.datasource.password}")
+    String password;
+    @Value("${spring.datasource.driver}")
+    String driver;
+
+    @Bean
+    public DataSource dataSource(){
+        HikariConfig config = new HikariConfig();
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+        config.setJdbcUrl(url);
+
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(1);
+        config.setPoolName("library-db-pool");
+        config.setMaxLifetime(600000);
+        config.setConnectionTimeout(100000);
+        config.setConnectionTestQuery("select 1");
+
+
+        return new HikariDataSource(config);
+    }
+
+}
